53573dbd405bde0433b68b56ba59897347f4b54c
Nolan Glore <nglore@microsoft.com>
2016-01-22 13:44:44 -0800

    V1 of the app.

---
+++ Source/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PipeToNotepad
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string filePath = String.Format("{0}.txt", Path.GetTempFileName());
+            using (var writer = new StreamWriter(new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None)))
+            {
+                string line;
+                while ((line = Console.ReadLine()) != null)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+
+            string app = args.Length > 0 ? args[0] : String.Empty;
+            string processName = null;
+            switch (app)
+            {
+                case "np":
+                    processName = "notepad++.exe";
+                    break;
+            }
+
+            if (processName != null)
+            {
+                Process.Start(processName, filePath);
+            }
+            else
+            {
+                Process.Start(filePath);
+            }
+        }
+    }
+}
